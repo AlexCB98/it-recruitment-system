ed5fb88206e39ba8edbd907cd8f2a260ce601790
Alexandru <lexyus22@gmail.com>
2026-05-03 11:36:37 +0200

    Add candidates data.

---
+++ data.py
@@ -0,0 +1,137 @@
+candidates = [
+    {
+        'name': 'Andrei Popescu',
+        'age': 24,
+        'experience_years': 2,
+        'github_projects': 8,
+        'salary_expectation': 3800,
+        'english_level': 8,
+        'test_score': 85
+    },
+    {
+        'name': 'James Mitchell',
+        'age': 29,
+        'experience_years': 5,
+        'github_projects': 15,
+        'salary_expectation': 6500,
+        'english_level': 10,
+        'test_score': 92
+    },
+    {
+        'name': 'Yuki Tanaka',
+        'age': 22,
+        'experience_years': 1,
+        'github_projects': 4,
+        'salary_expectation': 3200,
+        'english_level': 7,
+        'test_score': 70
+    },
+    {
+        'name': 'Elena Hoffmann',
+        'age': 31,
+        'experience_years': 7,
+        'github_projects': 20,
+        'salary_expectation': 8500,
+        'english_level': 9,
+        'test_score': 95
+    },
+    {
+        'name': 'Lucas Ferreira',
+        'age': 26,
+        'experience_years': 3,
+        'github_projects': 10,
+        'salary_expectation': 4800,
+        'english_level': 7,
+        'test_score': 78
+    },
+    {
+        'name': 'Sara Lindqvist',
+        'age': 28,
+        'experience_years': 4,
+        'github_projects': 12,
+        'salary_expectation': 5500,
+        'english_level': 9,
+        'test_score': 88
+    },
+    {
+        'name': 'Mihai Georgescu',
+        'age': 35,
+        'experience_years': 10,
+        'github_projects': 25,
+        'salary_expectation': 10000,
+        'english_level': 8,
+        'test_score': 90
+    },
+    {
+        'name': 'Priya Sharma',
+        'age': 23,
+        'experience_years': 1,
+        'github_projects': 6,
+        'salary_expectation': 3400,
+        'english_level': 9,
+        'test_score': 75
+    },
+    {
+        'name': 'Tom van der Berg',
+        'age': 27,
+        'experience_years': 4,
+        'github_projects': 11,
+        'salary_expectation': 5200,
+        'english_level': 8,
+        'test_score': 72
+    },
+    {
+        'name': 'Amara Diallo',
+        'age': 25,
+        'experience_years': 2,
+        'github_projects': 9,
+        'salary_expectation': 4200,
+        'english_level': 8,
+        'test_score': 88
+    },
+    {
+        'name': 'Kevin O\'Brien',
+        'age': 30,
+        'experience_years': 6,
+        'github_projects': 18,
+        'salary_expectation': 7200,
+        'english_level': 10,
+        'test_score': 91
+    },
+    {
+        'name': 'Lena Fischer',
+        'age': 26,
+        'experience_years': 3,
+        'github_projects': 13,
+        'salary_expectation': 5000,
+        'english_level': 8,
+        'test_score': 82
+    },
+    {
+        'name': 'Carlos Mendez',
+        'age': 32,
+        'experience_years': 8,
+        'github_projects': 22,
+        'salary_expectation': 8800,
+        'english_level': 7,
+        'test_score': 87
+    },
+    {
+        'name': 'Hana Novak',
+        'age': 24,
+        'experience_years': 2,
+        'github_projects': 7,
+        'salary_expectation': 3600,
+        'english_level': 8,
+        'test_score': 83
+    },
+    {
+        'name': 'Omar Khalil',
+        'age': 28,
+        'experience_years': 5,
+        'github_projects': 14,
+        'salary_expectation': 6200,
+        'english_level': 9,
+        'test_score': 89
+    },
+]
